1c0d042d204a8af3be952d196705b2b27932bba4
Mykyta Dniprovskyi <dnipromv@gmail.com>
2023-01-25 20:50:43 +0100

    improved scaling

--- src/app/components/scenes/MainScene.tsx
+++ src/app/components/scenes/MainScene.tsx
@@ -2,15 +2,24 @@ import { Container, Sprite, useApp } from "@pixi/react";
 import { useAssetsBundle } from "../../hooks/useAssets";
 import { useAppResize } from "../../hooks/useResize";
 
+const FIELD_WIDTH = 1024;
+const FIELD_HEIGHT = 768;
+
 export default function MainScene() {
     const { assets, progress, errorMessage } = useAssetsBundle("mainScene");
     const { width, height } = useAppResize();
+    const scale = Math.min(width / FIELD_WIDTH, height / FIELD_HEIGHT);
 
     return (
         <>
             {assets && (
-                <Container>
-                    <Sprite image="assets/textures/background.jpg" width={width} height={height} />
+                <Container
+                    scale={scale}
+                    x={width * 0.5}
+                    y={height * 0.5}
+                    pivot={{ x: FIELD_WIDTH * 0.5, y: FIELD_HEIGHT * 0.5 }}
+                >
+                    <Sprite image="assets/textures/background.jpg" />
                     <Sprite image="assets/textures/cardsDeck/BackFace.png" />
                 </Container>
             )}
